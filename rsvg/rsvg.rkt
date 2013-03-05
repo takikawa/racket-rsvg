6e1c1fd9465eed9eea160048b453b3b340695d15
Asumu Takikawa <asumu@ccs.neu.edu>
2013-03-05 12:14:14 -0500

    Make this an actual library

--- rsvg/rsvg.rkt
+++ rsvg/rsvg.rkt
@@ -1,24 +1,40 @@
 #lang racket
  
-;; Run in DrRacket to see the SVG logo
+;; FFI bindings to the RSVG library
  
 (require ffi/unsafe
          ffi/unsafe/define
          racket/draw
-         racket/draw/unsafe/cairo)
+         racket/draw/unsafe/cairo
+         (rename-in racket/contract
+                    [-> c:->]))
+
+(provide (contract-out
+          [load-svg-bitmap
+           (c:-> input-port? (is-a?/c bitmap%))]
+          [load-svg-from-file
+           (c:-> path-string? (is-a?/c bitmap%))]))
  
-;; you'll probably need to adjust this line a bit
 (define-ffi-definer define-rsvg (ffi-lib "librsvg-2" '("2" #f)))
  
+;; Data definitions
 (define-cpointer-type _RsvgHandle)
  
 (define-cstruct _GError
-  ([domain _int32] [code _int] [message _bytes]))
+  ([domain _int32] [code _int] [message _string]))
 (define-cstruct _RsvgDimensionData
   ([width _int] [height _int] [em _double] [ex _double]))
  
+;; FFI bindings
+
+;; functions for setting the DPI
 (define-rsvg rsvg_set_default_dpi (_fun _double -> _void))
- 
+(define-rsvg rsvg_set_default_dpi_x_y (_fun _double _double -> _void))
+(define-rsvg rsvg_handle_set_dpi (_fun _RsvgHandle _double -> _void))
+(define-rsvg rsvg_handle_set_dpi_x_y
+  (_fun _RsvgHandle _double _double -> _void))
+
+;; get the image dimensions for a handle
 (define-rsvg rsvg_handle_get_dimensions
   (_fun _RsvgHandle 
         (dim : _RsvgDimensionData-pointer = (make-RsvgDimensionData 0 0 0.0 0.0))
@@ -27,29 +43,41 @@
                    (RsvgDimensionData-height dim)
                    (RsvgDimensionData-em dim)
                    (RsvgDimensionData-ex dim))))
- 
+
+;; given an input array of bytes and its size, return a handle
+;; (or NULL) and an error object
 (define-rsvg rsvg_handle_new_from_data
   (_fun (input : _bytes)
         (input-length : _uint = (bytes-length input))
-        (_ptr i _GError)
+        (error : (_ptr o _GError-pointer))
         ->
-        _RsvgHandle))
+        (handle : _RsvgHandle/null)
+        ->
+        (values handle error)))
  
+;; render the handle to the given cairo_t context
 (define-rsvg rsvg_handle_render_cairo
   (_fun _RsvgHandle _pointer -> _bool))
- 
-;; set this to whatever SVG file path you want
-(define logo-bytes (file->bytes "plt-logo-red-shiny.svg"))
- 
-(define logo-handle (rsvg_handle_new_from_data logo-bytes (make-GError 0 0 #"")))
-(define-values (width height _1 _2)
-  (rsvg_handle_get_dimensions logo-handle))
- 
-(define bitmap (make-bitmap width height))
-(define bitmap-handle (send bitmap get-handle))
- 
-(rsvg_handle_render_cairo logo-handle (cairo_create bitmap-handle))
- 
-;; this is just a bitmap%, so you can turn it into a slideshow pict
-;; and manipulate it freely
-bitmap
+
+;; free the given handle
+(define-rsvg rsvg_handle_free (_fun _RsvgHandle -> _void))
+
+;; High-level API
+
+(define (load-svg-bitmap port)
+  (define input-bytes (port->bytes port))
+  (define-values (svg-handle error-obj)
+    (rsvg_handle_new_from_data input-bytes))
+  (unless svg-handle
+    (error (GError-message error-obj)))
+  (define-values (width height _1 _2)
+    (rsvg_handle_get_dimensions svg-handle))
+  (define bitmap (make-bitmap width height))
+  (define bitmap-handle (send bitmap get-handle))
+  (rsvg_handle_render_cairo svg-handle (cairo_create bitmap-handle))
+  (rsvg_handle_free svg-handle)
+  bitmap)
+
+(define (load-svg-from-file file-path)
+  (with-input-from-file file-path
+    (λ () (load-svg-bitmap (current-input-port)))))
