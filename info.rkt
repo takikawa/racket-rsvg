0fb78161c6eb10f93cd39c4de57dd4484c5ffdd8
Asumu Takikawa <asumu@ccs.neu.edu>
2013-09-20 16:46:04 -0400

    Add deps

--- info.rkt
+++ info.rkt
@@ -1,6 +1,7 @@
 #lang setup/infotab
 
 (define collection 'multi)
-(define deps '("base" "draw-lib"))
+(define deps '("base" "draw-lib" "gui-lib"))
+(define build-deps '("scribble-lib" "rackunit-lib" "scribble-lib" "pict-lib"))
 (define scribblings '(("rsvg/rsvg.scrbl")))
 
