1a2c82617db3b9dc4328b5ec3d88f9dd565e8288
Asumu Takikawa <asumu@ccs.neu.edu>
2013-10-02 17:45:04 -0400

    Update deps

--- info.rkt
+++ info.rkt
@@ -2,6 +2,7 @@
 
 (define collection 'multi)
 (define deps '("base" "draw-lib" "gui-lib"))
-(define build-deps '("scribble-lib" "rackunit-lib" "scribble-lib" "pict-lib"))
+(define build-deps '("scribble-lib" "rackunit-lib" "scribble-lib" "pict-lib"
+                     "draw-doc" "racket-doc" "slideshow-doc"))
 (define scribblings '(("rsvg/rsvg.scrbl")))
 
