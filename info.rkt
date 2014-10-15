6b2bbedb88d49709557c3e1515ff4e40a42b4383
Asumu Takikawa <asumu@ccs.neu.edu>
2014-10-15 01:34:08 -0400

    Fix package deps

--- info.rkt
+++ info.rkt
@@ -1,6 +1,6 @@
 #lang setup/infotab
 
 (define collection 'multi)
-(define deps '("base" "draw-lib" "gui-lib"))
+(define deps '("base" "draw-lib" "gui-lib" "pict-lib"))
 (define build-deps '("scribble-lib" "rackunit-lib" "scribble-lib" "pict-lib"
                      "draw-doc" "racket-doc" "slideshow-doc"))
