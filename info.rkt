1994a16f6150961cf4cb181353035aa176160f66
Asumu Takikawa <asumu@ccs.neu.edu>
2014-10-15 01:31:10 -0400

    Add docs to info.rkt in the correct way

--- info.rkt
+++ info.rkt
@@ -4,5 +4,3 @@
 (define deps '("base" "draw-lib" "gui-lib"))
 (define build-deps '("scribble-lib" "rackunit-lib" "scribble-lib" "pict-lib"
                      "draw-doc" "racket-doc" "slideshow-doc"))
-(define scribblings '(("rsvg/rsvg.scrbl")))
-
